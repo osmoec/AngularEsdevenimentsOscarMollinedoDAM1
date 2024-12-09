f174791ebea8d90cb7529d78eeb9d07616b8a937
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-09 11:42:27 +0100

    EX1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Implement} from '@angular/cli/lib/config/workspace-schema';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularEsdevenimentsOscarMollinedoDAM1';
+
+  ngOnInit(): void {
+  }
+
+  protected readonly canvifonsbttn = canvifonsbttn;
+  protected readonly canvifonsbttnNofocus = canvifonsbttnNofocus;
+}
+
+function canvifonsbttn(){
+  document.body.style.backgroundColor = 'blue'
+}
+
+function canvifonsbttnNofocus(){
+  document.body.style.backgroundColor = 'orange'
 }
