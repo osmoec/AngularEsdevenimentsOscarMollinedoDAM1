6e9741855586598cc8c147e79aaca975d8860159
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-10 09:42:45 +0100

    EX2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,29 @@ import {Implement} from '@angular/cli/lib/config/workspace-schema';
 export class AppComponent implements OnInit {
   title = 'AngularEsdevenimentsOscarMollinedoDAM1';
 
+  sumastring: string = ""
+  contadorB: number = 0
+
+  constructor() {}
+
   ngOnInit(): void {
+    document.body.addEventListener('keydown',(tecla: KeyboardEvent) =>{
+      let camp = document.getElementById("teclapresionada")
+      // @ts-ignore
+      camp.innerText = tecla.key
+      // @ts-ignore
+      camp.style.fontSize = '70px'
+    })
+    document.body.addEventListener('keyup',() =>{
+      // @ts-ignore
+      document.getElementById("teclapresionada").innerText = ""
+    })
   }
 
   protected readonly canvifonsbttn = canvifonsbttn;
   protected readonly canvifonsbttnNofocus = canvifonsbttnNofocus;
-}
 
+}
 function canvifonsbttn(){
   document.body.style.backgroundColor = 'blue'
 }
@@ -26,3 +42,5 @@ function canvifonsbttn(){
 function canvifonsbttnNofocus(){
   document.body.style.backgroundColor = 'orange '
 }
+
+
